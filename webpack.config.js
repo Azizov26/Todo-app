5990145aa00575f61e1e7ab764f864026918326e
Aziz <azizov-aziz58@yandex.ru>
2022-12-22 16:39:50 +0500

    webpack completed

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+let mode = 'development';
+let target = 'web';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
+}
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  new TsconfigPathsPlugin ({
+    configFile: './tsconfig.json'
+  }),
+  new ESLintPlugin({
+    extensions: ["js", "jsx", "ts", "tsx"],
+  }),
+];
+ 
+if(process.env.SERVE) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+}
+
+module.exports = {
+  mode,
+  target,
+  plugins,
+  devtool: 'source-map',
+  entry: './src/Index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+  devServer: {
+    static: {       
+      directory: path.resolve(__dirname, './dist')
+    }
+  },
+
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ['html-loader'] },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|ts)x?$/, 
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            },
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+  },
+}
