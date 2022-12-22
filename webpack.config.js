c90a32adc3874be738d6964d8ea449309a4482a6
Aziz <azizov-aziz58@yandex.ru>
2022-12-22 18:09:17 +0500

    eslint error/ bad work

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-let mode = 'development';
-let target = 'web';
+let mode = 'development'
+let target = 'web'
 if (process.env.NODE_ENV === 'production') {
-  mode = 'production';
-  target = 'browserslist';
+  mode = 'production'
+  target = 'browserslist'
 }
 
 const plugins = [
   new MiniCssExtractPlugin({
-    filename: '[name].[contenthash].css',
+    filename: '[name].[contenthash].css'
   }),
   new HtmlWebpackPlugin({
-    template: './src/index.html',
+    template: './src/index.html'
   }),
-  new TsconfigPathsPlugin ({
+  new TsconfigPathsPlugin({
     configFile: './tsconfig.json'
   }),
   new ESLintPlugin({
-    extensions: ["js", "jsx", "ts", "tsx"],
-  }),
-];
- 
-if(process.env.SERVE) {
-    plugins.push(new ReactRefreshWebpackPlugin());
+    extensions: ['js', 'jsx', 'ts', 'tsx']
+  })
+]
+
+if (process.env.SERVE) {
+  plugins.push(new ReactRefreshWebpackPlugin())
 }
 
 module.exports = {
@@ -40,10 +41,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     assetModuleFilename: 'assets/[hash][ext][query]',
-    clean: true,
+    clean: true
   },
   devServer: {
-    static: {       
+    static: {
       directory: path.resolve(__dirname, './dist')
     }
   },
@@ -57,27 +58,27 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
-          'sass-loader',
-        ],
+          'sass-loader'
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: mode === 'production' ? 'asset' : 'asset/resource',
+        type: mode === 'production' ? 'asset' : 'asset/resource'
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
-        test: /\.(js|ts)x?$/, 
+        test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
         use: {
-            loader: 'babel-loader',
-            },
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
 }
